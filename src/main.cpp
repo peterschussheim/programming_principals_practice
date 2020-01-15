2cac6bcec5c8d83833e8eae4d75a558a383b4d8e
Peter Schussheim <peter@schussheim.com>
2020-01-15 15:56:13 -0500

    refactor if-statements to switch-statements

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,36 @@
 // and a currency to convert from.
 // Ex: convert_to_usd(3.33, 'k')
 // currency can be either 'y' for yen or 'k' for kroner
+// Using switch-statements allows us to refactor and add/remove cases
+// more easily than using is-statements.
 int main() {
-  constexpr double y_to_usd = 0.0091;
+  constexpr double jpy_to_usd = 0.0091;
   constexpr double k_to_usd = 0.11;
+  constexpr double yuan_to_usd = 0.15;
+
   double amount = 1;
   char unit = ' ';
 
-  cout << "Please enter an amount followed by a unit (y for yen or k for kroner):\n";
+  cout << "Please enter an amount followed by a unit (j for yen or k for kroner or y for "
+          "yuan):\n";
   cin >> amount >> unit;
 
-  if (unit == 'y') {
-    cout << amount << " yen == $" << amount * y_to_usd << "\n";
-  } else if (unit == 'k') {
-    cout << amount << " kroner == $" << amount * k_to_usd << "\n";
-  } else {
-    cout << "Sorry, I don't a currency unit called '" << unit << "'\n";
+  switch (unit) {
+    case 'j': {
+      cout << amount << " yen == $" << amount * jpy_to_usd << "\n";
+      break;
+    }
+    case 'k': {
+      cout << amount << " kroner == $" << amount * k_to_usd << "\n";
+      break;
+    }
+    case 'y': {
+      cout << amount << " yuan == $" << amount * yuan_to_usd << "\n";
+      break;
+    }
+    default: {
+      cout << "Sorry, I don't a currency unit called '" << unit << "'\n";
+      break;
+    }
   }
 }
