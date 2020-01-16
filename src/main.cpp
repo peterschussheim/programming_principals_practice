ab3eb667ef70394b7c75c7ceaf3a3c9ef53ee554
Peter Schussheim <peter@schussheim.com>
2020-01-16 12:10:59 -0500

    implement square

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,21 @@
 #include "std_lib_facilities.h"
 
 using namespace std;
-// Using a while loop, write out a table of chars
-// and their int mapping.
-// EX:
-//
-// a  97
-// b  98
-// z  122
+
+int square(int x);
+
+int square(int x) {
+  int result = 0;
+  for (int count = x; count > 0; --count) {
+    result += x;
+  }
+
+  return result;
+}
+
 int main() {
-  // 48-57 for digits
-  cout << "Digits\n";
-  for (char start = 47; start < 57; ++start) {
-    cout << char(start + 1) << "\t" << int(start + 1) << "\n";
-  }
-
-  cout << "\n";
-  // 65 - 90 for uppercase
-  cout << "Uppercase Letters\n";
-  for (char start = 64; start < 90; ++start) {
-    cout << char(start + 1) << "\t" << int(start + 1) << "\n";
-  }
-
-  cout << "\n";
-  cout << "Lowercase Letters\n";
-  for (char start = 96; start < 122; ++start) {  // stop at char 'z'
-    cout << char(start + 1) << "\t" << int(start + 1) << "\n";
-  }
+  int val;
+  cout << "Enter an integer to square: \n";
+  cin >> val;
+  cout << "The square of " << val << " is " << square(val) << "\n";
 }
