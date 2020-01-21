a6ae1f08bba62dbb3eb35b7d8712f19194d70acd
Peter Schussheim <peter@schussheim.com>
2020-01-21 11:20:08 -0500

    complete drill step 8

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,27 @@
 using namespace std;
 
 int main() {
-  cout << "Enter a value including a valid unit."
-       << "EX: 12cm || 2.33 m || 9.032in || 44.03 ft\n";
-
-  cout << "When you are finished, enter '|' to exit the program\n";
   double dist;
   string unit;
 
+  cout << "Enter a value including a valid unit."
+       << "EX: 12cm || 2.33 m || 9.032in || 44.03 ft\n";
+  cout << "When you are finished, enter '|' to exit the program\n";
+
   while (cin >> dist >> unit) {
-    cout << "You entered: " << dist << " and a unit of: " << unit << "\n";
+    if (unit == "cm") {
+      cout << "You entered: " << dist << " and a unit of: " << unit << "\n";
+    } else if (unit == "m") {
+      cout << "You entered: " << dist << " and a unit of: " << unit << "\n";
+    } else if (unit == "in") {
+      cout << "You entered: " << dist << " and a unit of: " << unit << "\n";
+    } else if (unit == "ft") {
+      cout << "You entered: " << dist << " and a unit of: " << unit << "\n";
+    } else {
+      // Reject any unit not explicitly checked above
+      cout << "You entered an illegal unit format: " << unit << "\n";
+    }
   }
+
   keep_window_open();
 }
