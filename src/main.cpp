874f9f2393e9cab2625b5704f0dc2043ffc2f6f4
Peter Schussheim <peter@schussheim.com>
2020-01-21 11:03:06 -0500

    complete drill step 7

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,15 @@
 using namespace std;
 
 int main() {
-  cout << "Enter two integers, hit 'enter' after typing each\n";
-  double current;
-  cin >> current;
-  double largestSoFar = current;
-  double smallestSoFar = current;
-  while (cin >> current) {
-    if (current > largestSoFar) {
-      largestSoFar = current;
-      cout << current << " largest so far\n";
-    }
+  cout << "Enter a value including a valid unit."
+       << "EX: 12cm || 2.33 m || 9.032in || 44.03 ft\n";
 
-    if (current < smallestSoFar) {
-      smallestSoFar = current;
-      cout << current << " smallest so far\n";
-    }
+  cout << "When you are finished, enter '|' to exit the program\n";
+  double dist;
+  string unit;
+
+  while (cin >> dist >> unit) {
+    cout << "You entered: " << dist << " and a unit of: " << unit << "\n";
   }
+  keep_window_open();
 }
