2679fbc1c907d89a498d1471cbe1c6c402706c91
Peter Schussheim <peter@schussheim.com>
2020-01-27 13:56:15 -0500

    copy definition of main into ch file

--- src/ch7.cpp
+++ src/ch7.cpp
@@ -285,3 +285,27 @@ void calculate() {
       clean_up_mess();
     }
 }
+
+/**
+  int main() {
+  try {
+    // system-defined names:
+    define_name("pi", 3.1415926535);
+    define_name("e", 2.7182818284);
+    define_name("k", 1000);
+
+    calculate();
+
+    keep_window_open("~");
+    return 0;
+  } catch (exception& e) {
+    cerr << "exception: " << e.what() << endl;
+    keep_window_open("~");
+    return 1;
+  } catch (...) {
+    cerr << "exception\n";
+    keep_window_open("~");
+    return 2;
+  }
+}
+  */
