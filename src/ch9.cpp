37aea47b1182fe6e0a19c0c5b5283f3b178fad3c
Peter Schussheim <peter@schussheim.com>
2020-01-30 16:14:05 -0500

    complete drill part 1

--- src/ch9.cpp
+++ src/ch9.cpp
@@ -1,4 +1,3 @@
-
 /*
   Peter Schussheim
   01/30/20
@@ -17,16 +16,80 @@
     // 9.7.4
 */
 
-// void init_day(Date& dd, int y, int m, int d) {
-//  // check that (y,m,d) is valid date
-//  // if true, use it to intialize dd
-//}
-//
-// void Date::add_day(Date& dd, int n) {
-//  // increase dd by n days
-//  dd += n;
-//}
+#include "std_lib_facilities.h"
+
+struct Date {
+  int y;
+  int m;
+  int d;
+};
+
+void init_day(Date& dd, int y, int m, int d) {
+  // check that (y,m,d) is valid date
+  // if true, use it to intialize dd
+  if (d < 1 || d > 31) error("init_day: Invalid day");
+  if (m < 1 || m > 12) error("init_day: Invalid month");
+
+  dd.y = y;  // year (not checked yet)
+  dd.m = m;  // month in year
+  dd.d = d;  // day of month
+}
+
+void add_day(Date& dd, int n) {  // add or subtract days from a Date
+  // increase dd by n days
+  dd.d += n;
+
+  if (dd.d > 31) {  // day overflow
+    ++dd.m;
+    dd.d -= 31;
+  }
+  if (dd.d < 1) {  // day underflow
+    --dd.m;
+    dd.d += 31;
+  }
+  if (dd.m > 12) {  // month overflow
+    ++dd.y;
+    dd.m -= 12;
+  }
+  if (dd.m < 1) {  // month underflow
+    --dd.y;
+    dd.m += 12;
+  }
+}
+
+ostream& operator<<(ostream& os, const Date& d) {
+  return os << '(' << d.m << '/' << d.d << '/' << d.y << ')';
+}
 
 int main() {
-  return 0;
+  try {
+    Date today;
+    init_day(today, 1978, 6, 26);
+
+    Date tomorrow{today};  // copy today into tomorrow
+    add_day(tomorrow, 1);  // increment by 1 using add_day()
+
+    // output today and tomorrow using << defined in 9.8
+    cout << "Today: " << today << '\n';
+    cout << "Tomorrow: " << tomorrow << '\n';
+
+    Date test1;
+    init_day(test1, 2004, 13, -5);
+    cout << "test1: " << test1 << '\n';  // fails due to invalid month and day args
+
+    Date test2;
+    init_day(test2, 2004, 0, 5);  // fails due to invalid month and day args
+    cout << "test2: " << test2 << '\n';
+
+    keep_window_open("~");
+    return 0;
+  } catch (exception& e) {
+    cerr << e.what() << '\n';
+    keep_window_open("~");
+    return 1;
+  } catch (...) {
+    cerr << "Unknown exception\n";
+    keep_window_open("~");
+    return 2;
+  }
 }
