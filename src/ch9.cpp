a4d1982bc2247b015527eaa14adefc33f0388b01
Peter Schussheim <peter@schussheim.com>
2020-02-02 15:42:53 -0500

    complete part 4 of drill

--- src/ch9.cpp
+++ src/ch9.cpp
@@ -13,7 +13,7 @@
     // 9.4.1 DONE
     // 9.4.2 DONE
     // 9.4.3 DONE
-    // 9.7.1
+    // 9.7.1 DONE
     // 9.7.4
 */
 
@@ -30,12 +30,14 @@ Month operator+(const Month& m, int n) {
 }
 
 Month operator-(const Month& m, int n) { return (m + (-n)); }
+
 Month& operator+=(Month& m, int n) {
   m = m + n;
   return m;
 }
-bool operator<(const Month& m1, int d) { return true; }
-bool operator>(const Month& m1, int d) { return true; }
+
+bool operator<(const Month& m, int n) { return int(m) < n; }
+bool operator>(const Month& m, int n) { return int(m) > n; }
 
 Month& operator++(Month& m) {  // overloaded prefix ++
   m = static_cast<Month>(m + 1);
@@ -68,35 +70,38 @@ Date::Date(int yy, Month mm, int dd)  // constructor
 }
 
 void Date::add_day(int n) {  // add or subtract days from a Date
-  d += n;                    // increase d by n days
+
+  int n_d = n % 31;
+  int n_m = (n / 31) % 12;
+  int n_y = n / (31 * 12);
+
+  d += n_d;  // increase d by n days
 
   if (d > 31) {  // day overflow
-    ++m;
-    d -= 31;
+    ++n_m;       // increment month by 1
+    d -= 31;     // decrease the day member by 31
   }
   if (d < 1) {  // day underflow
-    --m;
+    --n_m;
     d += 31;
   }
-  if (m > 12) {  // month overflow
-    ++y;
-    m -= 12;
-  }
-  if (m < 1) {  // month underflow
-    --y;
-    m += 12;
-  }
+
+  if (Month::dec < (int(m) + n_m)) { ++n_y; }
+  if (Month::jan > (int(m) + n_m)) { --n_y; }
+
+  m += n_m;
+  y += n_y;
 }
 
 bool Date::is_valid() {  // validates day and month. Year validation not implemented
   if (d < 1 || d > 31) return false;
-  if (m < 1 || m > 12) return false;
+  if (m < Month::jan || m > Month::dec) return false;
 
   return true;
 }
 
 ostream& operator<<(ostream& os, const Date& d) {
-  return os << '(' << d.month() << '/' << d.day() << '/' << d.year() << ')';
+  return os << '(' << int(d.month()) << '/' << d.day() << '/' << d.year() << ')';
 }
 
 int main() {
@@ -107,8 +112,19 @@ int main() {
     cout << "Today: " << today << '\n';
     cout << "Tomorrow: " << tomorrow << '\n';
 
-    Date test1{2004, 1, -5};
-    cout << "test1: " << test1 << '\n';  // fails due to invalid month and day args
+    Date rollback{tomorrow};  // copy tomorrowa
+    rollback.add_day(-1);     // roll back to 12/31/2000
+    cout << "rollback " << rollback << '\n';
+
+    Date month_overflow{2000, Month::apr, 31};
+    cout << "month_overflow: " << month_overflow << '\n';
+
+    Date apr_to_may{month_overflow};
+    apr_to_may.add_day(2);
+    cout << "apr_to_may: " << apr_to_may << '\n';
+
+    // Date test1{2004, Month::jan, -5};
+    // cout << "test1: " << test1 << '\n';  // fails due to invalid month and day args
 
     // Date test2{2004, 0, 5};
     // cout << "test2: " << test2 << '\n';
@@ -122,10 +138,10 @@ int main() {
   } catch (Date::Invalid) {
     error("invalid date");
     keep_window_open("~");
-    return 1;
+    return 2;
   } catch (...) {
     cerr << "Unknown exception\n";
     keep_window_open("~");
-    return 2;
+    return 3;
   }
 }
