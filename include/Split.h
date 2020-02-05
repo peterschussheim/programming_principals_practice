78db330748bc7cf35e87a571ece401036b37c132
Peter Schussheim <peter@schussheim.com>
2020-02-05 16:42:59 -0500

    start working on exercise 11 and Punct_stream class

--- include/Split.h
+++ include/Split.h
@@ -5,19 +5,19 @@
   Peter Schussheim
   02/05/20
 
-  PPP Chapter 11 Exercise 10
+  PPP Chapter 11 Exercises 10 and 11
 */
 
 /*
-  Write a function vector<string> split(const string& s)
-  that returns a vector of whitespace-separated strings
-  from the argument s.
+  Exercise 10:
+    Write a function vector<string> split(const string& s)
+    that returns a vector of whitespace-separated strings
+    from the argument s.
 */
 
 /*
-  -
+  Basic split function.  Only supports "splitting" on plain space.
 */
-
 vector<string> split(const string& s) {
   vector<string> result;
   istringstream is{s};  // create a stream to read from s
@@ -29,3 +29,17 @@ vector<string> split(const string& s) {
 
   return result;
 }
+
+/*
+  A more useful variation of split which accepts a second argument
+  string& w which is a user-defined string of additional types
+  of "whitespace".
+
+  Example usage:
+  string test1 = "a man, a plan a (canal) panama!";
+  string ws = ".,!?();:";
+  vector<string> split_t = split(test1, ws);
+    -> contents of split_t = {"a", "man", "a", "plan", "a", "canal", "panama"}
+*/
+
+vector<string> split(const string& s, const string& w) { vector<string> result; }
