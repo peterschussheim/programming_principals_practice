d1bb3e76d0b8cd055db309f71a8a5eceb1517141
Peter Schussheim <peter@schussheim.com>
2020-02-17 13:45:34 -0500

    move headers into include dir

---
+++ src/ch14/ex1.cpp
@@ -0,0 +1,7 @@
+#include ""
+
+/*
+  Define two classes Smiley and Frowny, which are both derived from class
+  Circle and have two eyes and a mouth. Next, derive classes from Smiley and
+  Frowny which add an appropriate hat to each.
+*/
