ced8faadbdc3b87cc42897d4913f93388de100e0
Peter Schussheim <peter@schussheim.com>
2020-02-21 17:20:09 -0500

    complete drill part 2

--- src/ch15/drill.cpp
+++ src/ch15/drill.cpp
@@ -11,43 +11,74 @@
   4. Add double slope(double x) { return x/2; } to the window.
   5. Label the slope with a Text "x/2" at a point just above its bottom left end
       point.
+  6. Add double square(double x) { return x*x; } to the window.
+  7. Add a cosine to the window (don’t write a new function).
+  8. Make the cosine blue.
+  9. Write a function sloping_cos() that adds a cosine to slope() (as defined
+      above) and add it to the window.
 */
 
 double one(double x) { return 1; }
+double slope(double x) { return x / 2; }
+double square(double x) { return x * x; }
 
-void drill_part_1()
+void drill_part_2()
 {
+  using namespace Graph_lib;
   constexpr int xmax = 600;
   constexpr int ymax = 800;
   constexpr int x_orig = 300;
   constexpr int y_orig = 300;
   Point orig{x_orig, y_orig};
-  string label = "1 = = 20 pixels";
+
   constexpr int r_min = -10;  // range [-10:11)
   constexpr int r_max = 11;
   constexpr int n_points = 400;  // points used in range
-  constexpr int x_scale = 30;
-  constexpr int y_scale = 30;
+  constexpr int x_scale = 20;
+  constexpr int y_scale = 20;
   constexpr int xlength = 400;
   constexpr int ylength = 400;
 
-  Point tl{100, 100};
-  Simple_window win{tl, xmax, ymax, "Function graphs."};
-  Axis x{Axis::x, Point{20, y_orig}, xlength, xlength / x_scale, label};
-  Axis y{Axis::y, Point{x_orig, ylength + 20}, ylength, ylength / y_scale,
-         label};
-  x.set_color(Color::red);
-  y.set_color(Color::red);
-  win.attach(x);
-  win.attach(y);
+  Text sl{Point{100, y_orig + y_orig / 2 - 80}, "x/2"};
+  Simple_window win{Point{100, 100}, xmax, ymax, "Drill Part 2"};
+  Function f_one{one, r_min, r_max, orig, n_points, x_scale, y_scale};
+  Function f_slope{slope, r_min, r_max, orig, n_points, x_scale, y_scale};
+  Function f_square{square, r_min, r_max, orig, n_points, x_scale, y_scale};
+  Function cosine{[](double x) { return cos(x); },
+                  r_min,
+                  r_max,
+                  orig,
+                  n_points,
+                  x_scale,
+                  y_scale};
+  Function sloped_cosine{[](double x) { return cos(x) + slope(x); },
+                         r_min,
+                         r_max,
+                         orig,
+                         n_points,
+                         x_scale,
+                         y_scale};
+  sloped_cosine.set_color(Color::black);
+  cosine.set_color(Color::blue);
+  f_one.set_color(Color::black);
+  f_slope.set_color(Color::black);
+  f_square.set_color(Color::black);
+  sl.set_color(Color::black);
+
+  win.attach(f_one);
+  win.attach(sloped_cosine);
+  win.attach(f_slope);
+  win.attach(cosine);
+  win.attach(f_square);
+  win.attach(sl);
   win.wait_for_button();
 }
 
 int main()
 {
   try {
-    using namespace Graph_lib;
-    drill_part_1();
+    // drill_part_1();
+    drill_part_2();
     keep_window_open("~");
     return 0;
   }
@@ -62,3 +93,31 @@ int main()
     return 2;
   }
 }
+
+// void drill_part_1()
+//{
+//  constexpr int xmax = 600;
+//  constexpr int ymax = 800;
+//  constexpr int x_orig = 300;
+//  constexpr int y_orig = 300;
+//  Point orig{x_orig, y_orig};
+//  string label = "1 = = 20 pixels";
+//  constexpr int r_min = -10;  // range [-10:11)
+//  constexpr int r_max = 11;
+//  constexpr int n_points = 400;  // points used in range
+//  constexpr int x_scale = 30;
+//  constexpr int y_scale = 30;
+//  constexpr int xlength = 400;
+//  constexpr int ylength = 400;
+//
+//  Point tl{100, 100};
+//  Simple_window win{tl, xmax, ymax, "Function graphs."};
+//  Axis x{Axis::x, Point{20, y_orig}, xlength, xlength / x_scale, label};
+//  Axis y{Axis::y, Point{x_orig, ylength + 20}, ylength, ylength / y_scale,
+//         label};
+//  x.set_color(Color::red);
+//  y.set_color(Color::red);
+//  win.attach(x);
+//  win.attach(y);
+//  win.wait_for_button();
+//}
