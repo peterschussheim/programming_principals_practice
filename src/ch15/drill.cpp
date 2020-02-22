ef1bb7a6d41e56af7ef8c36d92ff95b7e268baad
Peter Schussheim <peter@schussheim.com>
2020-02-22 15:54:58 -0500

    complete chapter 15 drill

--- src/ch15/drill.cpp
+++ src/ch15/drill.cpp
@@ -74,11 +74,109 @@ void drill_part_2()
   win.wait_for_button();
 }
 
+/*
+  1. Define a struct Person containing a string name and an int age.
+  2. Define a variable of type Person , initialize it with “Goofy” and 63, and
+      write it to the screen ( cout ).
+  3. Define an input ( >> ) and an output ( << ) operator for Person ; read in a
+      Person from the keyboard ( cin ) and write it out to the screen ( cout ).
+  4. Give Person a constructor initializing name and age.
+  5. Make the representation of Person private, and provide const member
+      functions name() and age() to read the name and age.
+  6. Modify >> and << to work with the redefined Person.
+  7. Modify the constructor to check that age is [0:150) and that name doesn’t
+      contain any of the characters ; : " ' [ ] * & ^ % $ # @ ! . Use error() in
+      case of error. Test.
+  8. Read a sequence of Person s from input ( cin ) into a vector<Person> ;
+      write them out again to the screen ( cout ). Test with correct and errone-
+      ous input.
+  9. Change the representation of Person to have first_name and second_name
+      instead of name . Make it an error not to supply both a first and a second
+      name. Be sure to fix >> and << also. Test.
+*/
+
+struct Person {
+  Person() : f_name{""}, l_name{""}, a{1} {}
+  Person(string f_name, string l_name, int a);
+  string first_name() const { return f_name; }
+  string last_name() const { return l_name; }
+  int age() const { return a; }
+
+private:
+  string f_name;  // first name
+  string l_name;  // last name
+  int a;          // age
+};
+
+Person::Person(string fn, string ln, int aa)  // construct a person
+    : f_name{fn},
+      l_name{ln},
+      a{aa}
+{
+  for (int i = 0; i < fn.size(); ++i) {
+    if (!isalnum(fn[i]) || !isalpha(fn[i]))
+      error(
+          "name cannot contain any of the following characters: ; : \" \' \[ ] "
+          "\* \& \^ \% \$ \# \@ \! \.    ",
+          fn);
+  }
+  if (!(aa >= 0 && aa <= 150)) error("age must be in the range [0:150) ", aa);
+}
+
+ostream& operator<<(ostream& os, const Person& p);
+istream& operator>>(istream& is, Person& p);
+
+ostream& operator<<(ostream& os, const Person& p)  // write out a Person to cout
+{
+  return os << "Name: " << p.first_name() + " " + p.last_name() << "\n"
+            << "Age: " << p.age() << "\n";
+}
+
+istream& operator>>(istream& is, Person& p)
+{
+  // assume format: name, age
+  // Read in a Person from cin (keyboard)
+  string first_name, last_name;
+  int age;
+
+  is >> first_name >> last_name >> age;
+  if (!is) return is;  // return the stream if we have an error
+  // TODO: I couldn't figure out how to throw an error if a user
+  // fails to enter BOTH a first and last name.
+  // if (first_name.size() <= 1 || last_name.size() <= 1)
+  error("please enter a first AND last name");
+  p = Person{first_name, last_name, age};
+  return is;
+}
+
+void drill_part_3()
+{
+  // Person goofy{"Goofy", 1110};
+  // Person goofy{"Goof*y", 11}; // error
+  // Person goofy{";Goofy", 11};
+  // Person goofy{"G@oofy", 11};
+  // Person goofy{"Goofy #", 11};
+  // Person goofy{"Goofy!", 11};
+  // Person goofy{"Goofy?", 11};
+
+  // cin >> goofy;
+  // cout << goofy;
+}
+
 int main()
 {
   try {
     // drill_part_1();
-    drill_part_2();
+    // drill_part_2();
+    // drill_part_3();
+
+    vector<Person> p;  // read in from cin to p
+    cout << "Enter your first and last name and age, seperated by spaces\n";
+    Person s;
+    while (cin >> s) { p.push_back(s); }
+    cout << '\n';
+    for (Person per : p) { cout << per; }
+
     keep_window_open("~");
     return 0;
   }
@@ -121,3 +219,31 @@ int main()
 //  win.attach(y);
 //  win.wait_for_button();
 //}
+
+/*
+  UNUSED....ignore.  I thought the following would help but it made things
+  more complicated than necessary.
+
+  accepts an istream reference, a vector of Person objects and
+  a char containing the desired terminator char.
+
+  If we receive anything besides the expected Person type,
+  we update the stream state and attempt to gracefully
+  handle the char first checking if the char is the terminator
+  and if not, put the char BACK into the stream so that the
+  callee can use it and then we set the stream state to fail.
+void fill_vector(vector<Person> v, char terminator)
+{
+  for (Person p; std::cin >> p;) v.push_back(p);
+
+  // uh-oh, we've failed if we hit this part of fn
+  std::cin.clear();  // clear stream state
+  char c;
+  std::cin >> c;  // read a char
+
+  if (c != terminator) {
+    std::cin.unget();  // maybe my caller wants this char
+    std::cin.clear(ios_base::failbit);
+  }
+}
+*/
--- src/ch15/ex1.cpp
+++ src/ch15/ex1.cpp
@@ -2,70 +2,9 @@
 #include "Graph.h"
 #include "Simple_window.h"
 
-/*
-  Define two classes Smiley and Frowny, which are both derived from class
-  Circle and have two eyes and a mouth. Next, derive classes from Smiley and
-  Frowny which add an appropriate hat to each.
-*/
-
-/*
-  define an abstract class and try to define an object of that class.
-
-  Abstract Writing_utensil class to be a base class for specific
-  types of writing tools such as a pen, crayon, marker, pencil, etc.
-*/
-class Writing_utensil {
-public:
-  void virtual write_char() const = 0;  // write a single char to the screen
-
-private:
-  Point start;  // x, y coord of the point where we begin to write a char
-};
-
 int main()
 {
   try {
-    Point tl{100, 100};
-    Simple_window win{tl, 800, 800, "Smiley and Frowny"};
-
-    Smiley sm{Point{100, 100}, 50};
-    sm.set_color(Color::blue);
-    sm.set_style(Graph_lib::Line_style(Line_style::dashdot));
-
-    Frowny fr{Point{200, 100}, 50};
-    fr.set_color(Color::red);
-
-    Hat_smiley hs{Point{300, 100}, 50};
-    hs.set_color(Color::magenta);
-
-    Hat_frowny hf{Point{400, 100}, 50};
-    hf.set_color(Color::dark_green);
-
-    /*
-    Exercise #2: try to copy a shape
-    compiler error due to Shape's copy constructor being deleted
-    Hat_frowny hf_copy = hf;
-    */
-
-    /*
-      we cannot create an object of ann abstract class unless
-      ALL of the abstract class's virtual functions are overridden.
-    */
-    // Writing_utensil wu;
-
-    Immobile_circle ic{Point{100, 100}, 50};
-
-    ic.set_color(Color::black);
-
-    win.attach(ic);
-    win.wait_for_button();
-    // ic.move(300, 450); will not work since we've set Circle::move as private
-    /*win.attach(sm);
-    win.attach(fr);
-    win.attach(hf);
-    win.attach(hs);*/
-
-    win.wait_for_button();
     keep_window_open("~");
     return 0;
   }
