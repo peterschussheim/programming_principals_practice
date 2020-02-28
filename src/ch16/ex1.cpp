a13d4769f4d5f4a8d36acdc981e444df1e7e748c
Peter Schussheim <peter@schussheim.com>
2020-02-28 16:25:14 -0500

    comment out a few changes to try and understand how to solve ex2

--- src/ch16/ex1.cpp
+++ src/ch16/ex1.cpp
@@ -7,23 +7,45 @@ using namespace Graph_lib;
 using namespace std;
 
 /*
-  Make a My_window that’s a bit like Simple_window except that it has
-  two buttons, next and quit.
+  1) Make a My_window that’s a bit like Simple_window except that it has
+    two buttons, next and quit.
+
+  2) Make a window (based on My_window ) with a 4-by-4 checkerboard of
+    square buttons. When pressed, a button performs a simple action, such as
+    printing its coordinates in an output box, or turns a slightly different
+    color (until another button is pressed).
+
 */
 struct My_window : Graph_lib::Window {
   My_window(Point xy, int w, int h, const string& title);
-  bool button_pushed;
 
 private:
+  bool button_pushed;
+
+  int x_size = x_max();
+  int y_size = y_max();
+  int x_grid = 80;
+  int y_grid = 80;
+
   // widgets
   Button next_button;
   Button quit_button;
+  // Menu checker_board;
+  // Out_box coords;  // updates with relevant coordinates after a user clicks
 
   // actions
+
+  // updates Out_box coords with the position of the button clicked
+  // void print();
   void next();
   void quit();
 
   // callbacks
+
+  // cb that maps to member which updates out_box coords
+  // with the x,y coordinates of the specific button pressed.
+  // static void cb_print_coords(Address, Address);
+
   static void cb_next(Address, Address);
   static void cb_quit(Address, Address);
 };
@@ -32,9 +54,8 @@ private:
 
 My_window::My_window(Point xy, int w, int h, const string& title)
     : Window{xy, w, h, title},
-      button_pushed{false},
       next_button{Point{x_max() - 70, 30}, 70, 20, "Next", cb_next},
-      quit_button{Point{x_max() - 70, 100}, 70, 20, "Quit", cb_quit}
+      quit_button{Point{x_max() - 70, 50}, 70, 20, "Quit", cb_quit}
 {
   attach(next_button);
   attach(quit_button);
@@ -68,6 +89,7 @@ void My_window::next()
 
 //------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
 int main()
 {
   try {
