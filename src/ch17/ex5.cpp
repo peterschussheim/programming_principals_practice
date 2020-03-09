2626ee3b7449f77a84911bf0a757521e8107c5e6
Peter Schussheim <peter@schussheim.com>
2020-03-09 16:10:06 -0400

    start working on ex5

--- src/ch17/ex5.cpp
+++ src/ch17/ex5.cpp
@@ -1,10 +1,35 @@
-
+#include "Utils.h"
 
 /*
   Write a function, char* findx(const char* s, const char* x) , that finds the
   first occurrence of the C-style string x in s.
 */
+char* findx(const char* s, const char* x)
+{
+  // return null if unable to find
+  //
+}
 
-char* findx(const char* s, const char* x) {}
+int main()
+{
+  try {
+    const char* ss = "oband";
+    const char* xx = "ban";
 
-int main() { return 0; }
+    char* first_occurance = findx(ss, xx);
+    keep_window_open("~");
+    return 0;
+  }
+  catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    keep_window_open("~");
+    return 1;
+  }
+  catch (...) {
+    std::cerr << "unhandled error\n";
+    keep_window_open("~");
+    return 2;
+  }
+
+  return 0;
+}
