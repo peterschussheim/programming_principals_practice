ed2d642607687cb7f62af45fe160a0038f91f185
Peter Schussheim <peter@schussheim.com>
2020-03-09 14:49:18 -0400

    complete ex4

--- src/ch17/ex5.cpp
+++ src/ch17/ex5.cpp
@@ -1,1 +1,10 @@
+
+
+/*
+  Write a function, char* findx(const char* s, const char* x) , that finds the
+  first occurrence of the C-style string x in s.
+*/
+
+char* findx(const char* s, const char* x) {}
+
 int main() { return 0; }
