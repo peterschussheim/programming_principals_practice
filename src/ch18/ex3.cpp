b97b90f084727854c4f1caa65096b0d9056b0b47
Peter Schussheim <peter@schussheim.com>
2020-03-18 17:03:38 -0400

    prepare to start ex3

--- src/ch18/ex3.cpp
+++ src/ch18/ex3.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+/*
+  Write a function, int strcmp(const char* s1, const char* s2) , that compares
+  C-style strings. Let it return a negative number if s1 is lexicographically
+  before s2 , zero if s1 equals s2 , and a positive number if s1 is lexicograph-
+  ically after s2 . Do not use any standard library functions. Do not use
+  subscripting; use the dereference operator * instead.
+*/
+
+/*
+  compares two C-style strings at a time using lexicographic ordering.
+
+  Returns:
+    -1 if s1 < s2
+    0 if s1 == s2
+    1 if s1 > s2
+*/
+int strcmp(const char* s1, const char* s2) {}
+
 int main()
 {
   try {
