d672bb93e88ec11b227d09a38a037280562a84d0
Peter Schussheim <peter@schussheim.com>
2020-03-21 16:51:48 -0400

    complete exercise 6  # Please enter the commit message for your changes. Lines starting

--- src/ch18/ex6.cpp
+++ src/ch18/ex6.cpp
@@ -3,9 +3,27 @@
 
 using namespace std;
 
+/*
+  Modify cat_dot() from the previous exercise to take a string to be used as
+  the separator (rather than dot) as its third argument.
+*/
+string cat_dot(const string& s1, const string& s2, const string& sep = ".")
+{
+  string result = s1;
+  result += sep;
+  result += s2;
+  return result;
+}
+
 int main()
 {
   try {
+    string test_1 = cat_dot("Niels", "Bohr");
+    cout << "default separator: " << test_1 << '\n';
+
+    string test_2 = cat_dot("Niels", "Bohr", "*");
+    cout << "user-defined separator: " << test_2 << '\n';
+
     keep_window_open();
     return 0;
   }
