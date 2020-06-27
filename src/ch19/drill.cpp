4808e2a4ea0b7f140dacd09470f9f5b2abe6944e
Peter Schussheim <peter@schussheim.com>
2020-06-27 16:55:51 -0400

    working on bonus from drill

--- src/ch19/drill.cpp
+++ src/ch19/drill.cpp
@@ -5,15 +5,73 @@
 template<typename T> struct S {
   S(T v) : val{v} {}
   T& get();
-  void set(T v);
+  const T& get() const;
+  S& operator=(const T& s);
 
 private:
   T val;
 };
 
-template<typename T> T& S<T>::get() { return val; }
+template<class T> T& S<T>::get() { return val; }
+template<class T> const T& S<T>::get() const { return val; }
+template<class T> S<T>& S<T>::operator=(const T& s)
+{
+  if (val == s) return *this;
 
-template<typename T> void S<T>::set(T v) { val = v; }
+  val = s;
+  return *this;
+}
+
+template<class T> std::istream& operator>>(std::istream& is, S<T>& ss)
+{
+  T v{};
+  std::cin >> v;       // read into v
+  if (!is) return is;  // handle failure state
+  ss = v;              // what is this line doing?
+  return is;
+}
+
+template<class T> void read_val(T& v)  // read from cin to v
+{
+  std::cin >> v;
+}
+
+template<class T> std::istream& operator<<(std::ostream& os, std::vector<T>& v)
+{
+  os << "{ ";
+  for (int i = 0; i < v.size(); ++i) {
+    os << v[i];
+    if (i < v.size() - 1) os << ',';
+    os << ' ';
+  }
+
+  os << " }";
+  return os
+}
+
+template<class T> std::istream& operator>>(std::istream is, std::vector<T>& v)
+{
+  char ch1, ch2;
+  T temp;
+  std::vector<T> v_temp;
+  is >> ch1;
+  if (!is) return is;
+
+  if (ch1 != '{') {
+    is.clear(ios_base::failbit);
+    return is;
+  }
+  while (cin >> temp >> ch2 && ch2 == ',') { v_temp.push_back(temp); }
+  if (ch2 != '}') {
+    is.clear(ios_base::failbit);
+    return is;
+  }
+
+  v_temp.push_back(temp);  // read last line
+
+  // copy temp to prevent v from mutating on is failure
+  v = v_temp;
+}
 
 int main()
 {
@@ -23,12 +81,6 @@ int main()
   S<std::string> s_str{"Hello, world!"};
   S<std::vector<int>> s_v{{1, 2, 3, 4, 5}};
 
-  /*std::cout << s_int.val << '\n';
-  std::cout << s_char.val << '\n';
-  std::cout << s_double.val << '\n';
-  std::cout << s_str.val << '\n';
-  for (auto i : s_v.val) std::cout << i << ' ';
-  std::cout << '\n';*/
   std::cout << "Original values:\n";
   std::cout << s_int.get() << '\n';
   std::cout << s_char.get() << '\n';
@@ -38,21 +90,33 @@ int main()
   std::cout << '\n';
   std::cout << '\n';
 
-  // use S::set
-  s_int.set(99);
-  s_char.set('l');
-  s_double.set(99.33);
-  s_str.set("Bye, world!");
-  s_v.set({10, 9, 8, 7, 6});
+  std::cout << '\n';
+  /*S<int> s_int2{333};
+  s_int = s_int2;
+  std::cout << s_int.get() << '\n';
+  s_int = 90009;
+  s_char = 'l';
+  s_double = 99.33;
+  s_str = "Bye, world!";
+  s_v = {10, 9, 8, 7, 6};*/
 
+  std::cout << "\nenter a new int: ";
+  read_val(s_int);
+  std::cout << "\nenter a new char: ";
+  read_val(s_char);
+  std::cout << "\nenter a new double: ";
+  read_val(s_double);
+  std::cout << "\nenter a new string: ";
+  read_val(s_str);
   std::cout << "Modified values:\n";
   std::cout << s_int.get() << '\n';
   std::cout << s_char.get() << '\n';
   std::cout << s_double.get() << '\n';
   std::cout << s_str.get() << '\n';
-  for (auto el : s_v.get()) std::cout << el << ' ';
   std::cout << '\n';
 
-  // TODO: resume drill, part 10
+  read_val(s_v);
+  std::cout << s_v.get() << '\n';
+
   return 0;
 }
