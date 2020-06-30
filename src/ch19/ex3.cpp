c8741e15d01d12d7753c5408ffb64a8a68632c65
Peter Schussheim <peter@schussheim.com>
2020-06-30 17:27:06 -0400

    working on ex3 still

--- src/ch19/ex3.cpp
+++ src/ch19/ex3.cpp
@@ -17,10 +17,69 @@ public:
   Pair(T k, U val) : key{k}, value{val} {}
 };
 
+//------------------------------------------------------------------------------
+
+class Symbol_table {
+public:
+  double get_value(const string& s);       // get a value in sym_table
+  void set_value(string key, double val);  // modify value of an existing key
+  bool is_declared(string key);            // does this key already exist?
+  double define_name(string key, double value);  // create new entry in table
+private:
+  vector<Pair<string, double>> table;
+};
+
+//------------------------------------------------------------------------------
+
+double Symbol_table::get_value(const string& s)
+{
+  for (auto& element : table) {
+    if (element.key == s) { return element.value; }
+  }
+  std::cerr << "get: undefined variable " << s;
+}
+
+//------------------------------------------------------------------------------
+
+void Symbol_table::set_value(string key, double value)
+{
+  for (int i = 0; i < table.size(); ++i) {
+    if (table[i].key == key) {
+      table[i].value = value;
+      return;
+    }
+  }
+  std::cerr << "set: undefined variable " << key;
+}
+
+//------------------------------------------------------------------------------
+
+bool Symbol_table::is_declared(string key)  // check if key is already defined
+{
+  for (auto& element : table) {
+    if (element.key == key) { return true; }
+  }
+  return false;
+}
+
+//------------------------------------------------------------------------------
+double Symbol_table::define_name(string key, double value)
+{
+  if (is_declared(key)) std::cerr << key << " declared twice\n";
+  table.push_back(Pair<string, double>{key, value});
+  return value;
+}
+
 int main()
 {
   try {
-    Pair<string, double> p_1{"rate", 0.9};
+    // Pair<string, double> p_1{"rate", 0.9};
+    // double n = define_name(Pair{"timeout", 0.9});
+    Symbol_table s_table;
+    double pi = s_table.define_name("pi", 3.1415926535);
+    double my_e = s_table.define_name("e", 2.7182818284);
+    cout << "pi: " << pi << '\n';
+    cout << "my_e: " << my_e << '\n';
     return 0;
   }
   catch (const std::exception& e) {
