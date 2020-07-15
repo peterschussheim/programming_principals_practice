fba3dd78c3eff80c247de970a7b961e13fc390a5
Peter Schussheim <peter@schussheim.com>
2020-07-15 16:57:22 -0400

    wasted the whole day on bs

--- src/ch19/ex8.cpp
+++ src/ch19/ex8.cpp
@@ -1,12 +1,47 @@
 /*
-  Implement an allocator (§19.3.7) using the basic allocation functions mal-
-  loc() and free() (§B.11.4). Get vector as defined by the end of §19.4 to
-  work for a few simple test cases. Hint: Look up “placement new ” and
-  “explicit call of destructor” in a complete C++ reference.
+  Implement an allocator (§19.3.7) using the basic allocation functions malloc()
+  and free() (§B.11.4).
+
+  Get vector as defined by the end of §19.4 to work for a few simple test cases.
+
+  Hint: Look up “placement new ” and “explicit call of destructor” in a
+  complete C++ reference.
 */
 
 #include <iostream>
 #include <stdexcept>
+#include <memory>
+
+//------------------------------------------------------------------------------
+
+template<class T>
+class allocator {
+public:
+  T* allocate(int n);            // allocate space for n objects of type T
+  void deallocate(T* p, int n);  // destroy n objects of type T starting at p
+  void construct(T* p, const T& v);  // construct a T with the value v in p
+  void destroy(T* p);                // destroy the T in p
+};
+
+//------------------------------------------------------------------------------
+
+template<class T, class A = allocator<T>>
+class vector {
+  A alloc;    // allocate memory for vector elements
+  int sz;     // vector size
+  T* elem;    // pointer to vector elements
+  int space;  // size + free_space
+
+public:
+  vector() : sz{0}, elem{0}, space{0} {}
+  vector(int s);
+
+  vector(const vector&);  // copy constructor
+};
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 int main()
 {
