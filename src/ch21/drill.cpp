cc12e1076d10b763b074ea0a9efa41178e0a6c1a
Peter Schussheim <peter@schussheim.com>
2020-08-20 17:34:29 -0400

    clang-format appears to be working now

--- src/ch21/drill.cpp
+++ src/ch21/drill.cpp
@@ -1,28 +1,21 @@
 
 // After each operation (as defi ned by a line of this drill) print the vector .
-  //1. Define a struct Item { string name; int iid; double value; /* . . . */ };,
-  //  make a vector<Item> , vi , and fill it with ten items from a file.
-  //2. Sort vi by name.
-  //3. Sort vi by iid .
-  //4. Sort vi by value; print it in order of decreasing value (i.e., largest value
-  //first).
-  //5. Insert Item("horse shoe",99,12.34) and Item("Canon S400", 9988,499.95) .
-  //6. Remove (erase) two Item s identified by name from vi .
-  //7. Remove (erase) two Item s identified by iid from vi .
-  //8. Repeat the exercise with a list<Item> rather than a vector<Item>
-
+// 1. Define a struct Item { string name; int iid; double value; /* . . . */ };,
+//  make a vector<Item> , vi , and fill it with ten items from a file.
+// 2. Sort vi by name.
+// 3. Sort vi by iid .
+// 4. Sort vi by value; print it in order of decreasing value (i.e., largest
+// value first).
+// 5. Insert Item("horse shoe",99,12.34) and Item("Canon S400", 9988,499.95) .
+// 6. Remove (erase) two Item s identified by name from vi .
+// 7. Remove (erase) two Item s identified by iid from vi .
+// 8. Repeat the exercise with a list<Item> rather than a vector<Item>
 
 #include <iostream>
 #include <vector>
 #include <list>
 #include <algorithm>
 
-
-
 //------------------------------------------------------------------------------
 
-int main()
-{
-  
-  return 0;
-}
+int main() { return 0; }
