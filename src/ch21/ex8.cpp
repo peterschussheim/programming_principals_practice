bb24595cec1f26045c58c725c4edc9ef4ae9e9d3
Peter Schussheim <peter@schussheim.com>
2020-09-03 15:50:08 -0400

    done with ex8

--- src/ch21/ex8.cpp
+++ src/ch21/ex8.cpp
@@ -3,10 +3,44 @@
 // An example line would be 3: C++ rather than C++: 3
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <map>
+
+typedef std::pair<std::string, int> pair;  // save us from excessive typing
+
+//------------------------------------------------------------------------------
+
+struct Freq_asc {  // is x.second < y.second?
+
+  bool operator()(const pair& x, const pair& y) const
+  {
+    return x.second < y.second;
+  }
+};
+
+//------------------------------------------------------------------------------
 
 int main()
 {
   try {
+    std::map<std::string, int> words;
+    for (std::string s; std::cin >> s && s != "|";) ++words[s];
+
+    // since we cannot sort a map by value (only key), we need some additional
+    // data structure to copy each pair from the map which we can then sort
+    // using some custom comparator.
+    std::vector<pair> out;  // init vector of pairs
+    std::copy(words.begin(), words.end(),
+              std::back_inserter<std::vector<pair>>(out));
+
+    std::sort(out.begin(), out.end(), Freq_asc());
+    for (const auto& p : out) {
+      std::cout << p.second << ": " << p.first << '\n';
+    }
+
     return 0;
   }
   catch (const std::exception& e) {
