49ca342152287e85ba9eb263921d692ca35ff099
Peter Schussheim <peter@schussheim.com>
2020-10-08 16:35:35 -0400

    done with ex 4 and 5

--- src/ch23/ex4_5.cpp
+++ src/ch23/ex4_5.cpp
@@ -33,9 +33,49 @@ std::string find_sender(const Message* m);
 
 //------------------------------------------------------------------------------
 
-// TODO: Write up a summary of unordered_multimap vs multimap.
-// Mostly done with this.  Did some reading on benchmarking and evaluating
-// container performance and it is far from a trivial task
+// This was a fun exercise because it involved munging some data (interesting
+// on it's own) and comparing performance of two containers, std::multimap and
+// std::unordered_multimap.  The task was to find "a large email message file
+// (thousands of messages) and then time it as written with a multimap and with
+// that multimap replaced by an unordered_multimap".
+//
+// Without going on too much about preparing the data, it was a big of a pain
+// to source the data since commonly available data sets typically need some
+// processing to be usable.  In the case of the Enron email corpus, the emails
+// are organized into folders of selected senior executives mailboxes and
+// further organized into folders which vary depending on the individual.
+//
+// To make things simple, I produced a 6.81MB text file of emails from former
+// Enron co-CEO Ken Lay's "Documents" and "Inbox" mail folders, totalling about
+// 2500 messages.  So, this is not exactly large by many standards, however,
+// in order to complete this exercise in a reasonable amount of time, this
+// would be an acceptable tradeoff.  For reference, I initially downloaded an
+// archive of one of my personal mailboxes with around 560MB of data but
+// couldn't work efficiently with it after a few quick tests, mostly due to
+// the time taken to load the file into memory (about 3 minutes on very fast
+// hardware).
+//
+// I looked into benchmarking the program using google benchmark library (which
+// looks interesting, will try in the future) and even copy-pasting a simple
+// class using std::chrono but instead directly used std::chrono::stead_clock.
+// Not being an expert with regard performance measurement (and also relatively
+// new to C++), I do know that one needs many samples to get a reasonably useful
+// measurement, perhaps 5k+ followed by further regression analysis.  With that
+// said, I wasn't able to make any conclusions on performance of the previously
+// mentioned containers in the context of this experiment.
+//
+// I did find a few great articles on the relative performance of C++ containers
+// and when to use a particular one over another and some lesser-talked about
+// issues with their use.  To remind the reader, std::multimap is an ordered
+// container of key-value pairs (allowing duplicate keys) and is typically
+// implemented as a red-black tree. std::unordered_multimap differs in that
+// it does not gaurantee an order and under the hood, typically is implemented
+// as a hash table (array of linked lists).
+//
+// So, we can look at this exercise as a comparison between a sorted dataset
+// with O(log(n)) operations and an unsorted structure providing random-access
+// in constant time (O(1), amortized).
+
 int main()
 {
   try {
@@ -88,6 +128,7 @@ int main()
     std::cout << "Found the following messages from " << query << ":\n";
     std::cout << "-------------------------------------------------------------"
                  "-----------------\n";
+
     auto pp = sender.equal_range(query);
     int count = 1;  // print a count next to each result for readibility
     for (auto p = pp.first; p != pp.second; ++p) {
