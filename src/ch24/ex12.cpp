799f5127f54fa8641d1dded2b0ca874ecfdb3eb1
Peter Schussheim <peter@schussheim.com>
2020-11-22 17:12:20 -0500

    start working on exercises again

--- src/ch24/ex12.cpp
+++ src/ch24/ex12.cpp
@@ -7,10 +7,34 @@
 #include <iostream>
 
 namespace Numeric_lib {
-  Matrix<double> operator*(Matrix<double, 2>& lhs, Matrix<double>& rhs);
+  Matrix<double> operator*(Matrix<double, 2>& m, Matrix<double>& v)
+  {
+    if (m.dim1() != v.size() || m.dim2() != v.size()) {
+      error("matric vector multiplication dimension mismatch");
+    }
+    Matrix<double> res(v.size());
+    for (Index i = 0; i < res.size(); ++i) { res(i) = dot_product(m[i], v); }
+    return res;
+  }
 
   template<int N>
-  Matrix<double, N> operator+(Matrix<double, N>& lhs, Matrix<double, N>& rhs);
+  Matrix<double, N> operator+(Matrix<double, N>& m1, Matrix<double, N>& m2)
+  {
+    // use MUST ensure dimensions of m1 and m2 match
+
+    if (m1.size() != m2.size()) {
+      error("matrix addition requires identical dimension");
+    }
+
+    Matrix<double, n> m3 = m1;
+
+    double* ptr_2 = m3.data();
+    const double* ptr_3 = m2.data();
+
+    for (Index i = 0; i < m3.size(); ++i) { ptr_3[i] += ptr_2[i]; }
+
+    return m3;
+  }
 }
 
 //------------------------------------------------------------------------------
@@ -19,12 +43,17 @@ int main()
 {
   try {
     // 2 dim matrix of doubles:
-    Numeric_lib::Matrix<double, 2> m1(3, 4);
-    m1 = {
+    double m2_arr[][4] = {
         {00.1, 9, 3, 22.1},     // row 0
         {10.1, 0.2, 1.1, 9.1},  // row 1
         {50.1, 5.2, 5.1, 2.1}   // row 2
     };
+
+    Numeric_lib::Matrix<double, 2> m2 = m2_arr;
+    double v_arr[] = {221.9, 2.1, 44.3};
+    Numeric_lib::Matrix<double> v = v_arr;
+    Numeric_lib::Matrix<double> m2_v = m2 * v;
+    // TODO: Complete remainder of tests and move on to next exercise or ch.
 
     //// 1 dim matrix of doubles:
     // Numeric_lib::Matrix<double> m2{0.3, 1.2, 1, 0.2};
@@ -40,21 +69,3 @@ int main()
     return 2;
   }
 }
-
-//------------------------------------------------------------------------------
-
-// Multiplication operator for Matrix of doubles, 2 dimensions and a
-// one of one dimension
-// Numeric_lib::Matrix<double> operator*(Numeric_lib::Matrix<double, 2>& lhs,
-//                                      Numeric_lib::Matrix<double>& rhs)
-//{
-//  return Numeric_lib::Matrix<double>();
-//}
-//
-////------------------------------------------------------------------------------
-//
-// template<int N>
-// Numeric_lib::Matrix<double, N> operator+(Numeric_lib::Matrix<double, N>& lhs,
-//                                         Numeric_lib::Matrix<double, N>& rhs)
-//{
-//}
