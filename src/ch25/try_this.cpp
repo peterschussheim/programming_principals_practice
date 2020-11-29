e79223c00122af511d3f85ef89ed84560b838e31
Peter Schussheim <peter@schussheim.com>
2020-11-29 17:07:21 -0500

    working on Tiny Encryption Algorithm

--- src/ch25/try_this.cpp
+++ src/ch25/try_this.cpp
@@ -2,6 +2,7 @@
 #include <bitset>
 #include <iomanip>
 #include <sstream>
+#include <fstream>
 
 struct Message {
   int a;
@@ -172,13 +173,92 @@ void try_this_3()
   std::cout << '\n';
 }
 
+//------------------------------------------------------------------------------
+
+void encipher(const unsigned long* const v, unsigned long* const w,
+              const unsigned long* const k)
+{
+  static_assert(sizeof(long) == 4, "size of long wrong for TEA");
+
+  unsigned long y = v[0];
+  unsigned long z = v[1];
+  unsigned long sum = 0;
+  const unsigned long delta = 0x9E3779B9;
+
+  for (unsigned long n = 32; n-- > 0;) {
+    y += (z << 4 ^ z >> 5) + z ^ sum + k[sum & 3];
+    sum += delta;
+    z += (y << 4 ^ y >> 5) + y ^ sum + k[sum >> 11 & 3];
+  }
+
+  w[0] = y;
+  w[1] = z;
+}
+
+//------------------------------------------------------------------------------
+
+void decipher(const unsigned long* const v, unsigned long* const w,
+              const unsigned long* const k)
+{
+  static_assert(sizeof(long) == 4, "size of long wrong for TEA");
+  unsigned long y = v[0];
+  unsigned long z = v[1];
+  unsigned long sum = 0xC6EF3720;
+  const unsigned long delta = 0x9E3779B9;
+
+  // sum = delta<<5, in general sum = delta * n
+  for (unsigned long n = 32; n-- > 0;) {
+    z -= (y << 4 ^ y >> 5) + y ^ sum + k[sum >> 11 & 3];
+    sum -= delta;
+    y -= (z << 4 ^ z >> 5) + z ^ sum + k[sum & 3];
+  }
+
+  w[0] = y;
+  w[1] = z;
+}
+
+//------------------------------------------------------------------------------
+// TODO: complete this and write function to encrypt a file
+void decrypt(std::string infile, std::string outfile, std::string key)
+{
+  const int nchar = 2 * sizeof(long);  // 64 bits
+  const int kchar = 2 * nchar;         // 128 bits
+
+  while (key.size() < kchar) key += '0';  // pad key with 0
+
+  std::ifstream inf(infile);
+  std::ofstream outf(outfile);
+  if (!inf || !outf) std::cerr << "bad file name\n";
+
+  const unsigned long* k = reinterpret_cast<const unsigned long*>(key.data());
+
+  unsigned long* inptr[2];
+  char outbuf[nchar + 1];
+  outbuf[nchar] = 0;  // terminator
+
+  unsigned long* outptr = reinterpret_cast<unsigned long*>(outbuf);
+  inf.setf(std::ios_base::hex,
+           std::ios_base::basefield);  // use hexadecimal input
+
+  while (inf >> inptr[0] >> inptr[1]) {
+    decipher(inptr, outptr, k);
+    outf << outbuf;
+  }
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 int main()
 {
   // try_this_1();
   // try_this_2();
   // std::cout << "sizeof(signed char): " << sizeof(signed char) << '\n';
   // infinite();
-  try_this_3();
+  // try_this_3();
   // print_dec_to_hex();
+
+  // TODO: complete encryption program
+  decrypt("encrypted.txt", "decrypted.txt", "bs");
   return 0;
 }
