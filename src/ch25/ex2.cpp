5ed052870dc7771f685b84f84dde30b009ceae93
Peter Schussheim <peter@schussheim.com>
2020-12-23 17:31:31 -0500

    working on hex speak generator

--- src/ch25/ex2.cpp
+++ src/ch25/ex2.cpp
@@ -5,6 +5,7 @@
 //------------------------------------------------------------------------------
 
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <string>
 #include <algorithm>
@@ -13,20 +14,59 @@
 
 // substitution map
 // Hex characters: 0123456789ABCDEF
-//
-// clang-format off
-std::map<char,std::string> sub{
-  {'0', "o"},
-  {'1', "l"},
-  {'2', "to"},
-  {'3', ""}, // eliminate?
-  {'4', "for"},
-  {'5', "s"},
-  {'6', "g"}, // eliminate?
-  {'7', "T"},
-  {'8', "ate"},
-  {'9', "g"}
-};
-// clang-format on
+using Sub_map = std::map<std::string, std::string>;
 
-int main() { return 0; }
+Sub_map sub{
+    // clang-format off
+  {"o", "0"},
+  {"l", "1"},
+  {"to", "2"},
+  {"for", "4"},
+  {"s", "5"},
+  {"g", "6"},
+  {"T", "7"},
+  {"ate", "8"},
+  {"g", "9"}
+    // clang-format on
+};
+
+//------------------------------------------------------------------------------
+
+// Returns the hex substitution for a given sub_string.
+// Ex: auto replacement = get_hex_substitution("ate", sub); // "ate"
+std::string get_hex_substitution(std::string sub_string, Sub_map table)
+{
+  // TODO: Need to use a less stringent algorithm to handle partial matches
+  auto search = table.find(sub_string);
+  if (search != table.end()) { return search->second; }
+  else {
+    return "";  // if we cannot find a replacement in table, return empty string
+  }
+}
+
+//------------------------------------------------------------------------------
+
+// create ofstream "hex_speak.txt"
+// read dictionary into memory
+// attempt to build words from dictionary using only hex characters and
+// substitutions above.
+// write valid hex words to file
+
+void create_hex_dict(std::string infile, std::string outfile, Sub_map& s)
+{
+  std::ifstream inf(infile);
+  std::ofstream outf(outfile);
+  if (!inf || !outf) std::cerr << "bad file name\n";
+}
+
+//------------------------------------------------------------------------------
+
+int main()
+{
+  // create_hex_dict("usa_dict.txt", "hex_speak.txt", sub);
+
+  std::string ex_eng = "late";
+  std::string subs = get_hex_substitution(ex_eng, sub);
+
+  return 0;
+}
