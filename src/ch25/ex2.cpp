8242616253547b5ab570f331b3cac2ae6dd9c560
Peter Schussheim <peter@schussheim.com>
2020-12-26 17:14:03 -0500

    working towards a solution for hex word generator

--- src/ch25/ex2.cpp
+++ src/ch25/ex2.cpp
@@ -16,6 +16,8 @@
 // Hex characters: 0123456789ABCDEF
 using Sub_map = std::map<std::string, std::string>;
 
+constexpr int min_word_size = 3;
+
 Sub_map sub{
     // clang-format off
   {"o", "0"},
@@ -24,7 +26,7 @@ Sub_map sub{
   {"for", "4"},
   {"s", "5"},
   {"g", "6"},
-  {"T", "7"},
+  {"t", "7"},
   {"ate", "8"},
   {"g", "9"}
     // clang-format on
@@ -57,16 +59,35 @@ void create_hex_dict(std::string infile, std::string outfile, Sub_map& s)
   std::ifstream inf(infile);
   std::ofstream outf(outfile);
   if (!inf || !outf) std::cerr << "bad file name\n";
+
+  while (inf)  // while the list has words to read
+  {
+    std::string curr, hex;
+    inf >> curr;                         // read each line
+    if (curr.size() >= min_word_size) {  // ensure word meets min size
+      for (auto& search : sub) {
+        // look for the first occurence of a potential replaceable string
+        std::string::size_type idx = curr.find_first_of(search.first);
+        if (idx != std::string::npos) {  // if we found an occurence
+          // set hex to the string with the replacement
+          hex = search.second + std::string{curr, idx};
+          // hex = search.second + std::string{curr, search.first.size() - idx};
+        }
+      }
+    }
+    // TODO: Not done, need to add a condition when writing out hex words
+    outf << hex << '\n';
+  }
 }
 
 //------------------------------------------------------------------------------
 
 int main()
 {
-  // create_hex_dict("usa_dict.txt", "hex_speak.txt", sub);
+  create_hex_dict("usa_dict.txt", "hex_speak.txt", sub);
 
-  std::string ex_eng = "late";
-  std::string subs = get_hex_substitution(ex_eng, sub);
+  /* std::string ex_eng = "late";
+   std::string subs = get_hex_substitution(ex_eng, sub);*/
 
   return 0;
 }
