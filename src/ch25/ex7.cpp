626443faf6d82710d507b59c24686c5f9d3621bf
Peter Schussheim <peter@schussheim.com>
2021-01-05 16:12:51 -0500

    complete ex7

--- src/ch25/ex7.cpp
+++ src/ch25/ex7.cpp
@@ -1,2 +1,20 @@
 // Write out the hexadecimal values from 0 to 400; write out the hexadeci-
 // mal values from –200 to 200.
+
+#include <iostream>
+#include <iomanip>
+
+void print_hex(int from, int to)
+{
+  for (int i = from; i != to + 1; ++i) {
+    std::cout << std::setbase(10) << i << ": " << std::setbase(16) << i << '\n';
+  }
+}
+
+int main()
+{
+  print_hex(0, 400);
+  print_hex(-200, 200);
+
+  return 0;
+}
