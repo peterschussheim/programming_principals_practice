4834d3abb17223690b6e8a1c2a9f33e14845faa0
Peter Schussheim <peter@schussheim.com>
2021-01-11 17:20:23 -0500

    minimal working solution done

--- src/ch25/ex9.cpp
+++ src/ch25/ex9.cpp
@@ -7,6 +7,27 @@
 int main()
 {
   try {
+    // TODO: Write out a few notes on the code below.
+    // TODO: Write a few static assertions to validate the results below with
+    // <limits>.
+
+    int n = 0;
+    int i = 1;
+    while (true) {
+      std::cout << "bit " << n << " set: " << (i << n) << '\n';
+      if ((i << n) < (i << (n - 1))) break;
+      ++n;
+    }
+
+    std::cout << "int has " << n + 1 << " bits, shifting by " << n
+              << " positions.\n\n";
+
+    char ch = -1;
+    if (ch == -1)
+      std::cout << "char is signed\n";
+    else
+      std::cout << "char is unsigned\n";
+
     return 0;
   }
   catch (const std::exception& e) {
