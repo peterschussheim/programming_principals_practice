c85d914f0b45505666019ca6cc3d5f921fb216c2
Peter Schussheim <peter@schussheim.com>
2021-01-11 16:37:48 -0500

    done with ex8

--- src/ch25/ex8.cpp
+++ src/ch25/ex8.cpp
@@ -5,6 +5,26 @@
 int main()
 {
   try {
+    // print row by row
+    // qwertyuiop
+    // asdfghjkl
+    // zxcvbnm
+    // control chars
+
+    // function keys
+    for (unsigned char ch = 112; ch <= 126; ++ch) {
+      std::cout << int(ch) << ": " << ch << '\n';
+    }
+
+    // num/symbols
+    for (unsigned char ch = 33; ch <= 57; ++ch) {
+      std::cout << int(ch) << ": " << ch << '\n';
+    }
+    // uppercase alphabet
+    for (unsigned char ch = 65; ch <= 90; ++ch) {
+      std::cout << int(ch) << ": " << ch << '\n';
+    }
+
     return 0;
   }
   catch (const std::exception& e) {
